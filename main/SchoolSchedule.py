32dd6c9a01a2158af9c5d08e10596f12173a39a6
Shin-JaeHeon <heoni0504@gmail.com>
2017-09-02 10:41:02 +0900

    SchoolSchedule.py

---
+++ main/SchoolSchedule.py
@@ -0,0 +1,9 @@
+class SchoolSchedule:
+    def __init__(self):
+        self.schedule = ""
+
+    def __init__(self, schedule):
+        self.schedule = schedule
+
+    def to_string(self):
+        return self.schedule
